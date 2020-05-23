10ca5a073a800d80c5766d8cf94709cdeacf8414
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-24 01:38:28 +1000

    Began work on Rental model
    Not sure if necessary yet

---
+++ CAB301-Assignment/Models/Rentals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAB301_Assignment.Models
+{
+    class Rentals
+    {
+        public Movie Movie { get; set; }
+        public Member Member { get; set; }
+        public DateTime RentalDate { get; set; }
+
+        public Rentals (Movie movie, Member member)
+        {
+
+        }
+    }
+}
