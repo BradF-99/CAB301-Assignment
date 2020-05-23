cda70d1e09999b4669649a09c6fd6570a55901dc
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-23 13:56:42 +1000

    Implemented Address class and State enum

---
+++ CAB301-Assignment/Models/Member.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAB301_Assignment.Models
+{
+    public enum State
+    {
+        ACT,
+        NSW,
+        NT,
+        QLD,
+        SA,
+        TAS,
+        VIC,
+        WA
+    }
+    class Address
+    {
+        public int UnitNumber { get; set; }
+        public string PropertyNumber { get; set; } // string due to property numbers such as 19-21
+        public string StreetName { get; set; } 
+        public string Suburb { get; set; }
+        public State State { get; set; }
+
+        [Range(200, 9999, ErrorMessage = "Postcode must be between 0200 and 9999")]
+        public int Postcode { get; set; }
+    }
+
+    class Member
+    {
+
+    }
+}
