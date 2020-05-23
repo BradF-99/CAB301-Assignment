a44af6f51f22bb3e565b4bac7f4ebfce67f35e3f
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-23 14:01:49 +1000

    Added validation to Address class

--- CAB301-Assignment/Models/Member.cs
+++ CAB301-Assignment/Models/Member.cs
@@ -19,9 +19,17 @@ namespace CAB301_Assignment.Models
     class Address
     {
         public int UnitNumber { get; set; }
+
+        [Required(ErrorMessage = "Property number is required")]
         public string PropertyNumber { get; set; } // string due to property numbers such as 19-21
-        public string StreetName { get; set; } 
+
+        [Required(ErrorMessage = "Street name is required")]
+        public string StreetName { get; set; }
+
+        [Required(ErrorMessage = "Suburb is required")]
         public string Suburb { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
         public State State { get; set; }
 
         [Range(200, 9999, ErrorMessage = "Postcode must be between 0200 and 9999")]
