a176680dca6c73ad7bffb915edf46bc32d9cd9ca
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-23 14:46:33 +1000

    Added member class and removed validation for Address

--- CAB301-Assignment/Models/Member.cs
+++ CAB301-Assignment/Models/Member.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Text;
 
@@ -19,25 +18,37 @@ namespace CAB301_Assignment.Models
     class Address
     {
         public int UnitNumber { get; set; }
-
-        [Required(ErrorMessage = "Property number is required")]
         public string PropertyNumber { get; set; } // string due to property numbers such as 19-21
-
-        [Required(ErrorMessage = "Street name is required")]
         public string StreetName { get; set; }
-
-        [Required(ErrorMessage = "Suburb is required")]
         public string Suburb { get; set; }
-
-        [Required(ErrorMessage = "State is required")]
         public State State { get; set; }
 
-        [Range(200, 9999, ErrorMessage = "Postcode must be between 0200 and 9999")]
         public int Postcode { get; set; }
+
+        public Address (int unitNumber, string propertyNumber, string streetName, string suburb, State state, int postcode)
+        {
+            this.UnitNumber = unitNumber;
+            this.PropertyNumber = propertyNumber;
+            this.StreetName = streetName;
+            this.Suburb = suburb;
+            this.State = state;
+            this.Postcode = postcode;
+        }
+
+        public Address (string propertyNumber, string streetName, string suburb, State state, int postcode) // overload for no unit numebr
+        {
+            this.PropertyNumber = propertyNumber;
+            this.StreetName = streetName;
+            this.Suburb = suburb;
+            this.State = state;
+            this.Postcode = postcode;
+        }
     }
 
     class Member
     {
-
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
