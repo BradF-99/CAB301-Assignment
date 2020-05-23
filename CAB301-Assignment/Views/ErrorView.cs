7187005b91c789a0dda8fe211cec7e02bf8abb1a
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-23 21:20:27 +1000

    Added ErrorView Class

---
+++ CAB301-Assignment/Views/ErrorView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAB301_Assignment.Views
+{
+    class ErrorView
+    {
+        public static void Error(Exception e)
+        {
+            Error(e.Message);
+        }
+
+        public static void Error(string msg)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.Write("============ Error ===========\n" +
+                          "An error occurred when processing " +
+                          "your request.\n\n" + msg + "\n\n" +
+                          "Press any key to try again.");
+            Console.ReadKey();
+        }
+    }
+}
