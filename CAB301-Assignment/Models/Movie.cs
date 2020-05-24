52eaab4a9bcfe70b0d282bba566f2bcb59f67341
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-24 12:53:08 +1000

    Added duration field to Movie class

--- CAB301-Assignment/Models/Movie.cs
+++ CAB301-Assignment/Models/Movie.cs
@@ -30,17 +30,19 @@ namespace CAB301_Assignment.Models
         public string Title { get; set; }
         public List<string> Starring { get; set; }
         public string Director { get; set; }
+        public int Duration { get; set; }
         public Genre Genre { get; set; }
         public Classification Classification { get; set; }
         public DateTime ReleaseDate { get; set; }
         public int AmountBorrowed { get; set; }
         public int Copies { get; set; }
 
-        public Movie(string title, List<string> starring, string director, Genre genre, Classification classification, DateTime releaseDate, int copies)
+        public Movie(string title, List<string> starring, string director, int duration, Genre genre, Classification classification, DateTime releaseDate, int copies)
         {
             this.Title = title;
             this.Starring = starring;
             this.Director = director;
+            this.Duration = duration;
             this.Genre = genre;
             this.Classification = classification;
             this.ReleaseDate = releaseDate;
