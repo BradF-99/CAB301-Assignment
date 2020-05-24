ba950817b25ec66385505fae3afbe1da99af7fa4
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-24 22:28:37 +1000

    Fixed search and addition funcs

--- CAB301-Assignment/ViewModels/MemberCollection.cs
+++ CAB301-Assignment/ViewModels/MemberCollection.cs
@@ -9,16 +9,15 @@ namespace CAB301_Assignment.ViewModels
 {
     class MemberCollection
     {
-        private Member[] memberList;
+        private Member[] memberList = new Member[0]; // has a fit if initialised at 0
 
         public void MemberAdd(string firstName, string lastName, Address address, string phoneNumber, int password)
         {
             try
             {
-                int arrLength = memberList.Length;
                 Member newMember = new Member(firstName, lastName, address, phoneNumber, password);
-                Array.Resize(ref memberList, arrLength + 1);
-                memberList[arrLength - 1] = newMember;
+                Array.Resize(ref memberList, memberList.Length + 1);
+                memberList[^1] = newMember;
             }
             catch (Exception e)
             {
@@ -26,13 +25,30 @@ namespace CAB301_Assignment.ViewModels
             }
         }
 
+        public void MemberAdd(Member member)
+        {
+            try
+            {
+                Array.Resize(ref memberList, memberList.Length + 1);
+                memberList[^1] = member;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+
+
         public Member MemberSearch(string firstName, string lastName)
         {
             string userName = lastName + firstName;
             try
             {
-                Member result = memberList.SingleOrDefault(member => member.UserName == userName);
-                return result;
+                Member member = memberList.SingleOrDefault(member => member.UserName == userName);
+                if(member == null) {
+                    throw new System.ArgumentException("No result found. Please make sure your search query is correct.");
+                }
+                return member;
             }
             catch (Exception e)
             {
