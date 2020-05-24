7d0041a60f9fbb60134266359cf300b08dab85e3
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-24 21:35:42 +1000

    Removed unit number from Member class

--- CAB301-Assignment/Models/Member.cs
+++ CAB301-Assignment/Models/Member.cs
@@ -17,24 +17,13 @@ namespace CAB301_Assignment.Models
     }
     class Address
     {
-        public int UnitNumber { get; set; }
         public string PropertyNumber { get; set; } // string due to property numbers such as 19-21
         public string StreetName { get; set; }
         public string Suburb { get; set; }
         public State State { get; set; }
         public int Postcode { get; set; }
 
-        public Address (int unitNumber, string propertyNumber, string streetName, string suburb, State state, int postcode)
-        {
-            this.UnitNumber = unitNumber;
-            this.PropertyNumber = propertyNumber;
-            this.StreetName = streetName;
-            this.Suburb = suburb;
-            this.State = state;
-            this.Postcode = postcode;
-        }
-
-        public Address (string propertyNumber, string streetName, string suburb, State state, int postcode) // overload for no unit number
+        public Address (string propertyNumber, string streetName, string suburb, State state, int postcode)
         {
             this.PropertyNumber = propertyNumber;
             this.StreetName = streetName;
