ab2c1c9a9dc4763214c7a6ebadb2ce8b483ecadc
Brad Fuller <37586752+BradF-99@users.noreply.github.com>
2020-05-25 12:54:04 +1000

    Implemented Member login and movie borrow / return

--- CAB301-Assignment/Views/MenuView.cs
+++ CAB301-Assignment/Views/MenuView.cs
@@ -12,6 +12,7 @@ namespace CAB301_Assignment.Views
     class MenuView
     {
         readonly MemberCollection memberCollection = new MemberCollection();
+        readonly MovieCollection movieCollection = new MovieCollection();
 
         public void MainMenu()
         {
@@ -57,10 +58,10 @@ namespace CAB301_Assignment.Views
             string userName = Console.ReadLine();
             Console.Write("Password: ");
             string password = Console.ReadLine();
-            if(userName == "staff" && password == "today123")
+            if (userName == "staff" && password == "today123")
             {
                 this.StaffMainMenu();
-            } 
+            }
             else
             {
                 ErrorView.Error("Invalid username and/or password. " +
@@ -76,14 +77,14 @@ namespace CAB301_Assignment.Views
             string userName = Console.ReadLine();
             Console.Write("Password: ");
             string password = Console.ReadLine();
-            if (userName == "staff" && password == "today123") // fix
+            try
             {
-                this.MemberMainMenu();
+                Member member = memberCollection.MemberLogin(userName, password);
+                MemberMainMenu(member);
             }
-            else
+            catch (Exception e)
             {
-                ErrorView.Error("Invalid username and/or password. " +
-                    "Please check your credentials and try again.");
+                ErrorView.Error(e);
             }
         }
 
@@ -98,7 +99,7 @@ namespace CAB301_Assignment.Views
                           " 0. Return to main menu\n" +
                           "==============================\n\n" +
                           "Please make a selection (1-4, or 0 to return to main menu): ");
-            
+
             string menuChoice = Console.ReadLine();
 
             try
@@ -144,7 +145,7 @@ namespace CAB301_Assignment.Views
         {
             Console.Clear();
             Console.Write("==========Add Movie==========\n");
-            
+
             // Title
             Console.Write("Title: ");
             string title = Console.ReadLine();
@@ -176,7 +177,7 @@ namespace CAB301_Assignment.Views
             int genreLength = Enum.GetValues(typeof(Genre)).Length;
             for (int i = 0; i < genreLength; i++)
             {
-                Console.Write(Enum.GetName(typeof(Genre),i) + " ");
+                Console.Write(Enum.GetName(typeof(Genre), i) + " ");
             }
 
             while (!genreValidInput)
@@ -246,7 +247,10 @@ namespace CAB301_Assignment.Views
                 switch (reviewChoice.ToLower())
                 {
                     case "y":
-                        // add db
+                        Console.Clear();
+                        Console.Write("Please wait...");
+                        Movie newMovie = new Movie(title, starringList, director, duration, genre, classification, releaseDate, copies);
+                        movieCollection.Tree.Insert(newMovie);
                         StaffMainMenu();
                         break;
                     case "n":
@@ -261,7 +265,7 @@ namespace CAB301_Assignment.Views
 
         public void StaffRemoveMovie()
         {
-
+            // TO DO IMPLEMENT
         }
 
         public void StaffAddMember()
@@ -323,8 +327,8 @@ namespace CAB301_Assignment.Views
                 Console.Write("\nPostcode: ");
                 string pcodeInput = Console.ReadLine();
                 bool pcodeValidParse = int.TryParse(pcodeInput, out postcode);
-                
-                if(pcodeValidParse && postcode >= 200 && postcode <= 9999)
+
+                if (pcodeValidParse && postcode >= 200 && postcode <= 9999)
                 {
                     pcodeValidInput = true;
                 }
@@ -371,8 +375,8 @@ namespace CAB301_Assignment.Views
                     case "y":
                         Console.Clear();
                         Console.Write("Please wait...");
-                        Address address = new Address(propertyNumber,streetName,suburb,state,postcode);
-                        memberCollection.MemberAdd(firstName,lastName,address,phoneNumber,password);
+                        Address address = new Address(propertyNumber, streetName, suburb, state, postcode);
+                        memberCollection.MemberAdd(firstName, lastName, address, phoneNumber, password);
                         // add db
                         StaffMainMenu();
                         break;
@@ -399,7 +403,7 @@ namespace CAB301_Assignment.Views
             {
                 Member result = memberCollection.MemberSearch(firstName, lastName);
                 Console.Write("==========Member Lookup==========\n" +
-                "Results: Phone number for " + firstName + " " + lastName + " is " + result.PhoneNumber + 
+                "Results: Phone number for " + firstName + " " + lastName + " is " + result.PhoneNumber +
                 "\nPress any key to return to the main menu.");
                 Console.ReadKey();
             }
@@ -410,57 +414,170 @@ namespace CAB301_Assignment.Views
 
         }
 
-        public void MemberMainMenu()
+        // MEMBER MENUS
+        public void MemberMainMenu(Member member)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("==========Member Menu==========\n" +
+                              "Welcome, " + member.FirstName + ". Select from the options below.\n\n" +
+                              " 1. Display all movies\n" +
+                              " 2. Borrow a movie DVD\n" +
+                              " 3. Return a movie DVD\n" +
+                              " 4. List current borrowed movie DVDs\n" +
+                              " 5. Display top 10 most popular movies\n" +
+                              " 0. Return to main menu\n" +
+                              "==============================\n\n" +
+                              "Please make a selection (1-5, or 0 to return to main menu): ");
+                string menuInput = Console.ReadLine();
+                bool menuValidInput = int.TryParse(menuInput, out int menuChoice);
+
+                if (menuValidInput)
+                {
+                    switch (menuChoice)
+                    {
+                        case 1:
+                            MemberDisplayAllMovies();
+                            break;
+                        case 2:
+                            MemberBorrowMovie(member);
+                            break;
+                        case 3:
+                            MemberReturnMovie(member);
+                            break;
+                        case 4:
+                            MemberListBorrowed(member);
+                            break;
+                        case 5:
+                            MemberDisplayPopular();
+                            break;
+                        case 0:
+                            MainMenu();
+                            break;
+                        default:
+                            ErrorView.Error("Invalid selection. Please choose 1, 2, 3, 4, 5 or 0.");
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void MemberDisplayAllMovies()
+        {
+            // TO DO IMPLEMENT
+        }
+
+        public void MemberBorrowMovie(Member member)
         {
             Console.Clear();
-            Console.Write("==========Member Menu==========\n" +
-                          " 1. Display all movies\n" +
-                          " 2. Borrow a movie DVD\n" +
-                          " 3. Return a movie DVD\n" +
-                          " 4. List current borrowed movie DVDs\n" +
-                          " 5. Display top 10 most popular movies\n" +
-                          " 0. Return to main menu\n" +
-                          "==============================\n\n" +
-                          "Please make a selection (1-5, or 0 to return to main menu): ");
-            
-            string menuChoice = Console.ReadLine();
+            Console.Write("==========Borrow Movie==========\n" +
+                "Movie Name: ");
+            string movieName = Console.ReadLine();
 
             try
             {
-                int result = Int32.Parse(menuChoice);
-                switch (result)
+                Console.Clear();
+                Console.Write("Please wait...");
+                Movie result = movieCollection.Tree.Search(movieName, null);
+
+                bool reviewValidInput = false;
+                while (!reviewValidInput)
                 {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                    case 0:
-                        MainMenu();
-                        break;
-                    default:
-                        ErrorView.Error("Invalid selection. Please choose 1, 2, 3, 4, 5 or 0.");
-                        MemberMainMenu();
-                        break;
+                    Console.Clear();
+                    Console.Write("==========Borrow Movie==========\n" +
+                        "Please review the below information to ensure everything is correct.\n" +
+                        "\n Title: " + result.Title +
+                        "\n Starring: " + String.Join(",", result.Starring) +
+                        "\n Director: " + result.Director +
+                        "\n Duration: " + result.Duration + " mins" +
+                        "\n Genre: " + result.Genre.ToString() +
+                        "\n Classification: " + result.Classification.ToString() +
+                        "\n Release Date: " + result.ReleaseDate.ToString("dd/MM/yyyy") +
+                        "\n\nTo confirm borrowing this movie, press Y. To start again, press N.");
+                    string reviewChoice = Console.ReadLine();
+
+                    switch (reviewChoice.ToLower())
+                    {
+                        case "y":
+                            Console.Clear();
+                            Console.Write("Please wait...");
+                            if (member.RentedMovies.Count >= 10)
+                                throw new Exception("You have reached your quota of borrowed movies. " +
+                                "Please return some movies before attempting to borrow more.");
+                            movieCollection.Tree.BorrowMovie(result);
+                            member.RentedMovies.Add(result);
+
+                            return;
+                        case "n":
+                            return;
+                    }
+
+                    // no need for default as it will loop again if invalid response
+                    Console.Beep();
                 }
             }
             catch (Exception e)
             {
-                if (e is FormatException)
-                {
-                    ErrorView.Error("Invalid selection. Please choose 1, 2, 3, 4, 5 or 0.");
-                }
-                else
-                {
-                    ErrorView.Error(e.Message);
-                }
-                MemberMainMenu();
+                ErrorView.Error(e);
             }
+        }
+
+        public void MemberReturnMovie(Member member)
+        {
+            if (member.RentedMovies.Count == 0)
+                throw new Exception("You have no movies to return.");
+
+            Console.Clear();
+            Console.Write("==========Return Movie==========\n" +
+                "Here are your currently borrowed movies. Please select the one you wish to return or type 0 to exit.\n\n");
+
+            Movie returnChoice = null;
+            int choice = 0;
+            for (int i = 0; i < member.RentedMovies.Count; i++)
+            {
+                Console.WriteLine(i + 1 + member.RentedMovies[i].Title);
+            }
+            bool returnChoiceValidInput = false;
+            while (!returnChoiceValidInput)
+            {
+                string choiceInput = Console.ReadLine();
+                bool choiceValidParse = Int32.TryParse(choiceInput, out choice);
+
+                if (choiceValidParse)
+                {
+                    if (choice == 0)
+                    {
+                        return;
+                    }
+                    if (choice <= member.RentedMovies.Count)
+                    {
+                        returnChoice = member.RentedMovies[choice - 1];
+                    }
+                }
+            }
+
+            try
+            {
+                Console.Clear();
+                Console.Write("Please wait...");
+                movieCollection.Tree.ReturnMovie(returnChoice);
+                member.RentedMovies.RemoveAt(choice - 1);
+            }
+            catch (Exception e)
+            {
+                ErrorView.Error(e);
+            }
+        }
+
+        public void MemberListBorrowed(Member member)
+        {
+
+        }
+
+        public void MemberDisplayPopular()
+        {
+
         }
     }
 }
